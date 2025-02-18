a83f52c635b95afe5fbb17b1a991afe0c1612ece
Inquisitiveshri34 <diyashrivastava34@gmail.com>
2025-02-18 10:56:47 +0530

    Enhanced

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,10 +1,19 @@
-import React,{useContext} from 'react'
+import React, {useContext} from 'react'
 import { UserContext } from '../context/user.context'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     const { user } = useContext(UserContext)
   return (
-    <div>{JSON.stringify(user)}</div>
+    <>
+      { (user) ? 
+        <div>{JSON.stringify(user)}</div> : 
+        <>
+        <Link to="/register" className="text-blue-500 block hover:underline">Register</Link>
+        <Link to="/login" className="text-blue-500 block hover:underline">Login</Link>
+        </> 
+      }
+    </>
   )
 }
 
